595da0a6093dafce8d9fa31dc5e628f2ce58d335
PurelyStupidity <micoolplays@gmail.com>
2022-05-13 11:33:22 +1200

    May 13, 2022 11:33 AM

---
+++ Enter_Budget/Enter_Budget_V2.py
@@ -0,0 +1,14 @@
+# ask user for budget and if its an interger then convert it to float.
+def check_float(number):
+    try:
+        float(number)
+        return True
+    except ValueError:
+        return False
+
+budget = input("Enter your budget: $")
+while check_float(budget) == False:
+    print("Please enter a valid number")
+    budget = input("Enter your budget: $")
+budget = float(budget)
+print(budget)
