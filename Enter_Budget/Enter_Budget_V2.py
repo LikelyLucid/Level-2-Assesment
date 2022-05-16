0f1db72cb0b2861ffbe7cbd7fa5abe297ff1dcfe
PurelyStupidity <micoolplays@gmail.com>
2022-05-16 13:39:39 +1200

    May 16, 2022 1:39 PM

--- Enter_Budget/Enter_Budget_V2.py
+++ Enter_Budget/Enter_Budget_V2.py
@@ -7,7 +7,6 @@ def check_float(question):
             return float(float_num)
 
         except ValueError:
-            
             print("Please enter a valid number")
 
 print(check_float("What is your budget? "))
