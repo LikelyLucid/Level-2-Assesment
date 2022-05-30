60570bb445fc8e4c4192802db1dc98fda1f6a463
PurelyStupidity <micoolplays@gmail.com>
2022-05-30 20:21:37 +1200

    May 30, 2022 8:21 PM

--- 02_Check_Product_name/Enter_product_Name_V3.py
+++ 02_Check_Product_name/Enter_product_Name_V3.py
@@ -1,8 +1,11 @@
 # ask user for Product name and if its empty then ask again but allow for spaces in the text and numbers
+from pyparsing import alphanums
+
+
 def Check_Blank(question):
     while True:
         text = input(question) # ask question
-        if text != "" or text == al: # if text isn't empty then return it
+        if text != "" or text == alphan: # if text isn't empty then return it
             return text
         else: # if text is empty then ask again
             print("Please enter a valid name.")
