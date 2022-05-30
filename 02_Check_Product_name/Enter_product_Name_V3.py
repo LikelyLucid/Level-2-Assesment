4fd2694c878b7119ed31157fc59fbd89743f4610
PurelyStupidity <micoolplays@gmail.com>
2022-05-30 20:22:19 +1200

    May 30, 2022 8:22 PM

--- 02_Check_Product_name/Enter_product_Name_V3.py
+++ 02_Check_Product_name/Enter_product_Name_V3.py
@@ -1,12 +1,6 @@
 # ask user for Product name and if its empty then ask again but allow for spaces in the text and numbers
 
 
-
-from curses.ascii import isalnum
-
-from pyparsing import alphanums
-
-
 def Check_Blank(question):
     while True:
         text = input(question) # ask question
