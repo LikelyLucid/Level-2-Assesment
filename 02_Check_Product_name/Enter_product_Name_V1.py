b7eb548ed7992dcf6c4b3083589cf145c89b9273
PurelyStupidity <micoolplays@gmail.com>
2022-05-30 14:18:32 +1200

    May 30, 2022 2:18 PM

--- 02_Check_Product_name/Enter_product_Name_V1.py
+++ 02_Check_Product_name/Enter_product_Name_V1.py
@@ -1,5 +1,5 @@
 # ask user for Product name and if its empty then ask again.
-def Check_Blank(question): # define function Check_Blank
+def Check_Blank(question): 
     while True:
         text = input(question)
         if text.isalpha() == True:
