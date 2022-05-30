ef259ae40b5e0c8c98e847001986a271f5e47a9c
PurelyStupidity <micoolplays@gmail.com>
2022-05-30 13:49:50 +1200

    May 30, 2022 1:49 PM

---
+++ 02_Check_Product_name/Enter_product_Name_V1.py
@@ -0,0 +1,13 @@
+# ask user for budget and if its an interger then convert it to float.
+def check_float(question):
+    while True:
+        try:
+            float_num = input(question)
+            return float(float_num)
+
+        except ValueError:
+            print("Please enter a valid number")
+
+
+while True:
+    print(check_float("What is your budget? "))
