99787a0676f1eba5cee5bf3e9fdaf03907ec01e2
PurelyStupidity <micoolplays@gmail.com>
2022-05-30 13:55:31 +1200

    May 30, 2022 1:55 PM

--- 02_Check_Product_name/Enter_product_Name_V2.py
+++ 02_Check_Product_name/Enter_product_Name_V2.py
@@ -1,4 +1,4 @@
-# ask user for Product name and if its empty then ask again.
+# ask user for Product name and if its empty then ask again but allow for spaces in the text
 def Check_Blank(question):
     while True:
         text = input(question)
