f576e3f3731fcefdac8e34272ac21bf38c89efa9
PurelyStupidity <micoolplays@gmail.com>
2022-05-30 14:18:06 +1200

    May 30, 2022 2:18 PM

--- 01_Enter_Budget/Enter_Budget_V2.py
+++ 01_Enter_Budget/Enter_Budget_V2.py
@@ -5,7 +5,7 @@ def check_float(question):
             float_num = input(question) # ask question
             return float(float_num) # convert to float and return it
 
-        except ValueError:
+        except ValueError: # if not a float then ask again
             print("Please enter a valid number")
 
 
