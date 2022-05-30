a4d9c266ac1cd7dfca34353283b32a90ae19fe9a
PurelyStupidity <micoolplays@gmail.com>
2022-05-30 14:18:42 +1200

    May 30, 2022 2:18 PM

--- 02_Check_Product_name/Enter_product_Name_V1.py
+++ 02_Check_Product_name/Enter_product_Name_V1.py
@@ -1,8 +1,8 @@
 # ask user for Product name and if its empty then ask again.
 def Check_Blank(question):
     while True:
-        text = input(question) 
-        if text.isalpha() == True:
+        text = input(question) # ask question
+        if text.isalpha() == True: 
             return text
         else:
             print("Please enter a valid name.")
