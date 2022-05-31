6273405c2f7a91f3189a346e8ea217be357abac7
PurelyStupidity <micoolplays@gmail.com>
2022-05-31 14:13:51 +1200

    May 31, 2022 2:13 PM

--- 03_Enter_compare_unit/gwgogo.py
+++ 03_Enter_compare_unit/gwgogo.py
@@ -5,7 +5,7 @@ for i in range 5:
     value1 = random.randint(1, 10)
     value2 = random.randint(1, 10)
     value3 = random.randint(1, 10)
-    list.append()
+    list.append([])
 high = 0
 for item in list:
     if
