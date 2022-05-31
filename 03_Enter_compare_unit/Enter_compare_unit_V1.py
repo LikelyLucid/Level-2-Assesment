faa81874041d9c895fe9d1ace2fb87bf0b170a5e
PurelyStupidity <micoolplays@gmail.com>
2022-05-31 13:53:59 +1200

    May 31, 2022 1:53 PM

--- 03_Enter_compare_unit/Enter_compare_unit_V1.py
+++ 03_Enter_compare_unit/Enter_compare_unit_V1.py
@@ -2,6 +2,6 @@
 1. Ask user to enter a Unit
 2. Check if unit contains numbers, if so then continue to step 4
 3. if not then go to step 1
-4. Get letters from the unit, e.g "kg" from "1kg" or "
+4. Get letters from the unit, e.g "kg" from "1kg" or "1L" from "1L"
 
 """
