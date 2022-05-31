368c1594a7ce176e2e6d7fedc944a4f6a97f6dc4
PurelyStupidity <micoolplays@gmail.com>
2022-05-31 13:45:03 +1200

    May 31, 2022 1:45 PM

--- 03_Enter_compare_unit/Enter_compare_unit_V1.py
+++ 03_Enter_compare_unit/Enter_compare_unit_V1.py
@@ -1,1 +1,3 @@
 #ask user to enter unit, check if unit contains numbers, if it doesnt then ask again, if it does then continue, get the letters from unit e.g L or KG, if there is no unit then ask user to enter again. if the unit is ml or g then convert it to L or KG.
+
+def 
