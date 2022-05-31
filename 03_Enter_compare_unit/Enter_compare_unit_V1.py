0f0b14df53fffe12de04d245f4847d2b28f3329f
PurelyStupidity <micoolplays@gmail.com>
2022-05-31 13:43:55 +1200

    May 31, 2022 1:43 PM

--- 03_Enter_compare_unit/Enter_compare_unit_V1.py
+++ 03_Enter_compare_unit/Enter_compare_unit_V1.py
@@ -1,1 +1,1 @@
-#ask user to enter unit, check if unit contains numbers, if it doesnt then ask again, if it does then continue, get the letters from unit,
+#ask user to enter unit, check if unit contains numbers, if it doesnt then ask again, if it does then continue, get the letters from unit e.g 
