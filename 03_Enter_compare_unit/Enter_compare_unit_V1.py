85f37e6b0aafc427519367a6bf103ad0b7197b99
PurelyStupidity <micoolplays@gmail.com>
2022-05-31 13:55:10 +1200

    May 31, 2022 1:55 PM

--- 03_Enter_compare_unit/Enter_compare_unit_V1.py
+++ 03_Enter_compare_unit/Enter_compare_unit_V1.py
@@ -4,5 +4,5 @@
 3. if not then go to step 1
 4. Get letters from the unit, e.g "kg" from "1kg" or "L" from "1L"
 5. If letters are empty then go to step 1
-6. If unit is "mg" or "g" or "ml" then convert to Kg
+6. If unit is "mg" or "g" or "ml" then convert to Kg and L respectively
 """
