265fae47ab176f4d9823af1ccfd12a02935a07de
PurelyStupidity <micoolplays@gmail.com>
2022-05-31 14:02:34 +1200

    May 31, 2022 2:02 PM

--- 03_Enter_compare_unit/Enter_compare_unit_V1.py
+++ 03_Enter_compare_unit/Enter_compare_unit_V1.py
@@ -16,7 +16,7 @@ def check_unit(unit):
     while True:
         if (char.isdigit() for char in unit):
             print("has numbers")
-            if "Kg""L" or "mg" or "g" or "ml" in unit:
+            if "Kg" or "L" or "mg" or "g" or "ml" in unit:
                 if "mg"
 
 
