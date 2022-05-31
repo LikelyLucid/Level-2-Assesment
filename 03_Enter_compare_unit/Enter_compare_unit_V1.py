c201cabb9ba5c099aded3f4ea13ac8393749a935
PurelyStupidity <micoolplays@gmail.com>
2022-05-31 13:56:10 +1200

    May 31, 2022 1:56 PM

--- 03_Enter_compare_unit/Enter_compare_unit_V1.py
+++ 03_Enter_compare_unit/Enter_compare_unit_V1.py
@@ -1,10 +1,9 @@
 """
 1. Ask user to enter a Unit
-2. Check if unit contains numbers, if so then continue to step 4
+2. Check if unit has numbers, if so then continue to step 4
 3. if not then go to step 1
 4. Get letters from the unit, e.g "kg" from "1kg" or "L" from "1L"
 5. If letters are empty then go to step 1
 6. If unit is "mg" or "g" or "ml" then convert to Kg and L respectively
 """
 def check_unit(unit):
-    
