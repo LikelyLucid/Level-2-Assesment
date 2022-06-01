d62321fee7cf5279876b7f80aaca53f3f169a3e8
PurelyStupidity <micoolplays@gmail.com>
2022-06-02 10:18:09 +1200

    June 2, 2022 10:18 AM

--- 03_Enter_compare_unit/Enter_compare_unit_V1.py
+++ 03_Enter_compare_unit/Enter_compare_unit_V1.py
@@ -1,7 +1,3 @@
-def input():
-    unit = input("Enter the unit\nKg, L, ml, g: ").lower()
-    amount = input("enter the amount: ")
-    return unit, amount
 
 def get_unit():
     unit = input("Enter the unit\nKg, L, ml, g: ").lower()
