1cb11c3c1792eeda23cb53c1282935fe0d15070f
PurelyStupidity <micoolplays@gmail.com>
2022-06-02 10:05:21 +1200

    June 2, 2022 10:05 AM

--- 03_Enter_compare_unit/Enter_compare_unit_V1.py
+++ 03_Enter_compare_unit/Enter_compare_unit_V1.py
@@ -13,4 +13,5 @@ an expected input is 15mg and the expected output is 0.015kg
 def check_unit(unit):
     #check if unit has numbers in it
     if unit.isdigit():
-        # 
+        # check if unit has letters in it
+        if
