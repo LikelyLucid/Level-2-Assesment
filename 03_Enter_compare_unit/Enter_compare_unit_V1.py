f8eedaab2b3a73e10750c27802450ff0f717edcd
PurelyStupidity <micoolplays@gmail.com>
2022-06-02 09:47:43 +1200

    June 2, 2022 9:47 AM

--- 03_Enter_compare_unit/Enter_compare_unit_V1.py
+++ 03_Enter_compare_unit/Enter_compare_unit_V1.py
@@ -22,5 +22,5 @@ def check_unit(unit):
                     unit = int(unit) / 1000
                     print(unit)
                     return unit
-                else
+                else: return unit
 check_unit("15mg")
