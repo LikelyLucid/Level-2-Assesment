9d2a5a6039a3571f7f8b1f47e7ff67dfb5c283a2
PurelyStupidity <micoolplays@gmail.com>
2022-06-02 10:20:51 +1200

    June 2, 2022 10:20 AM

--- 03_Enter_compare_unit/Enter_compare_unit_V1.py
+++ 03_Enter_compare_unit/Enter_compare_unit_V1.py
@@ -4,3 +4,4 @@ def get_unit():
         unit = input("Enter the unit\nKg, L, ml, g: ").lower()
         if unit != "kg" and unit != "l" and unit != "ml" and unit != "g":
             print("Invalid unit")
+        else: 
