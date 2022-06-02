308090dfc60a40730866d258de7ba8b9770dbdfd
PurelyStupidity <micoolplays@gmail.com>
2022-06-03 11:24:49 +1200

    June 3, 2022 11:24 AM

---
+++ 03_Enter_compare_unit/Enter_compare_unit_V2.py
@@ -0,0 +1,23 @@
+def get_unit():
+
+    while True:
+        unit = input("Enter the unit\nKg, L, ml, g: ").lower()
+        if unit not in ["kg", "l", "ml", "g"]:
+            print("\nInvalid unit\n")
+        else:
+            break
+
+    while True:
+        amount = input("Enter the amount: ")
+        if amount.isnumeric():
+            break
+        else:
+            print("\nInvalid amount\n")
+
+    if unit in ["ml", "g"]:
+        amount = int(amount) / 1000
+
+    return unit, amount
+
+
+print(get_unit())
