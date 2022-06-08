ded87f821b17d4353f7a4757073cf22cc75113d7
PurelyStupidity <micoolplays@gmail.com>
2022-06-09 09:47:55 +1200

    June 9, 2022 9:47 AM

--- 04_Enter_Price/Enter_Price_V1.py
+++ 04_Enter_Price/Enter_Price_V1.py
@@ -2,4 +2,7 @@ def check_price():
     while True:
         price = float(input("Enter the price: "))
         #if the price contains numbers then it will return the price
+        if price > 0:
+            return price
+        #if the price is not a number then it will ask the user to enter a number again
         
