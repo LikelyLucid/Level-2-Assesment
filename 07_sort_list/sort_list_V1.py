7217cb1a1187db53c2b9ee7df161182b040c56a3
PurelyStupidity <micoolplays@gmail.com>
2022-06-10 20:01:20 +1200

    June 10, 2022 8:01 PM

--- 07_sort_list/sort_list_V1.py
+++ 07_sort_list/sort_list_V1.py
@@ -1,3 +1,3 @@
-list = [("banana", "g", "1", "1.50"),("coffee", "g", "1", "1.50")]
+list = [("banana", "g", "1", "1.50"),("coffee", "g", "1", "3")]
 def sort_list():
     # Sort list by unit price to find cheapest – ascending order – formatted
