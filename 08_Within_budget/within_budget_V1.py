5cbdd0431a7333d2b79be12f65ef2852a04e1489
PurelyStupidity <micoolplays@gmail.com>
2022-06-10 20:59:37 +1200

    June 10, 2022 8:59 PM

--- 08_Within_budget/within_budget_V1.py
+++ 08_Within_budget/within_budget_V1.py
@@ -8,6 +8,6 @@ list = [('banana', 'g', '3', '1.50', 0.5), ('Tuy', 'g', '2', '5', 2.5), ('coffee
 def sort_list_budget(list, budget):
     for item in list:
         cost = float(item[3])
-        print([item[3])
+        print(cost)
 
 sort_list_budget(list, budget)
