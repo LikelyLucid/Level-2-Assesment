a7a6f344cb04c8031c514b2019cce648e3672afc
PurelyStupidity <micoolplays@gmail.com>
2022-06-10 17:42:55 +1200

    June 10, 2022 5:42 PM

--- 05_Append_to_list/Append_to_list.py
+++ 05_Append_to_list/Append_to_list.py
@@ -2,4 +2,11 @@ import json
 
 def append_list(Item, Unit, Weight, Price):
     #append items to Items.json
-    
+    data = {
+        "Item": Item,
+        "Unit": Unit,
+        "Weight": Weight,
+        "Price": Price
+    }
+    with open('Items.json', 'a') as outfile:
+        json.dump(data, outfile, indent=4)
