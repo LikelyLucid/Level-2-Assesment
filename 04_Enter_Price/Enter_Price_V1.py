e219e243f24d7ad52761cbc1c527fa6812f82b62
PurelyStupidity <micoolplays@gmail.com>
2022-06-10 14:58:11 +1200

    June 10, 2022 2:58 PM

--- 04_Enter_Price/Enter_Price_V1.py
+++ 04_Enter_Price/Enter_Price_V1.py
@@ -2,7 +2,7 @@ def check_price():
     while True:
         try:
             price = float(input("Enter the price: $"))
-            break
+            return price
         except ValueError:
             print("\nInvalid price\n")
 
