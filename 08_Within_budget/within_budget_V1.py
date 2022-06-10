ef280137c25d51c69d0add586c4e46da93b56f13
PurelyStupidity <micoolplays@gmail.com>
2022-06-10 20:56:49 +1200

    June 10, 2022 8:56 PM

--- 08_Within_budget/within_budget_V1.py
+++ 08_Within_budget/within_budget_V1.py
@@ -3,13 +3,5 @@ budget = 8
 list = [('banana', 'g', '3', '1.50', 0.5), ('Tuy', 'g', '2', '5', 2.5), ('coffee', 'g', '1', '3', 3.0), ('apple', 'g', '1', '9', 9)]
 
 def get_inside_budget(list, budget):
-    new_list = []
-    for item in list:
-        price = float(item[3])
-        weight = float(item[2])
-        total_price = (price/weight)
-        item = item + (total_price,)
-        new_list.append(item)
-    new_list.sort(key=lambda x: x[4])
-    return new_list
+    for item
 
