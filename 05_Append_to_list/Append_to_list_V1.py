a2176f8d48b0d7836a4eb14ea0674c5b5eeb6a41
PurelyStupidity <micoolplays@gmail.com>
2022-06-10 18:05:44 +1200

    June 10, 2022 6:05 PM

--- 05_Append_to_list/Append_to_list_V1.py
+++ 05_Append_to_list/Append_to_list_V1.py
@@ -1,6 +1,6 @@
 import json
 
-def append_list(Item, Unit, Weight, Price):
+def append_list(Item, Unit, Weight, Price): 
     #append items to Items.json
     data = {
         "Item": Item,
@@ -12,5 +12,5 @@ def append_list(Item, Unit, Weight, Price):
         json.dump(data, outfile, indent=4) # writes data to file
 
 append_list('Banana', 'g', '1', '1.50')
-file = json.load(open('Items.json')) # opens file in append mode
+file = json.load(open('Items.json')) # opens file
 print(file)
