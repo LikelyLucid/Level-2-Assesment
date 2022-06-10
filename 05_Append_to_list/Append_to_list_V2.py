88a972ffc45a691eedd01f7e42e9b83e01ca9147
PurelyStupidity <micoolplays@gmail.com>
2022-06-10 21:02:43 +1200

    June 10, 2022 9:02 PM

--- 05_Append_to_list/Append_to_list_V2.py
+++ 05_Append_to_list/Append_to_list_V2.py
@@ -1,18 +1,20 @@
 import csv
 
+
 def append_list(Item, Unit, Weight, Price):
-    #append items to Items.csv
+    # append items to Items.csv
     data = {
         "Item": Item,
         "Unit": Unit,
         "Weight": Weight,
-        "Price": Price
-    } #data is a dictionary
-    with open('Items.csv', 'a', newline="") as outfile: # opens file in append mode
+        "Price": Price,
+    }  # data is a dictionary
+    with open("Items.csv", "a", newline="") as outfile:  # opens file in append mode
         writer = csv.DictWriter(outfile, fieldnames=data.keys())
-        writer.writerow(data) # writes data to file
+        writer.writerow(data)  # writes data to file
+
 
 # used for testing
-append_list('Banana', 'g', '1', '1.50')
-append_list('Apple', 'g', '1', '1.50')
-append_list('Orange', 'g', '1', '1.50')
+append_list("Banana", "g", "1", "1.50")
+append_list("Apple", "g", "1", "1.50")
+append_list("Orange", "g", "1", "1.50")
