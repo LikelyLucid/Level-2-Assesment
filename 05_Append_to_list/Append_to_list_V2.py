ec7b00d9026937973c521db79a14ed4a744a9ed0
PurelyStupidity <micoolplays@gmail.com>
2022-06-10 17:59:12 +1200

    June 10, 2022 5:59 PM

--- 05_Append_to_list/Append_to_list_V2.py
+++ 05_Append_to_list/Append_to_list_V2.py
@@ -8,4 +8,5 @@ def append_list(Item, Unit, Weight, Price):
         "Price": Price
     }
     with open('Items.csv', 'a') as outfile:
-        
+        writer = csv.writer(outfile)
+        writer.writerow(data.values())
