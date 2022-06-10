318d0e011caa8bc8aafe45574ad7aa6924dbb01c
PurelyStupidity <micoolplays@gmail.com>
2022-06-10 21:02:27 +1200

    June 10, 2022 9:02 PM

--- 08_Within_budget/within_budget_V1.py
+++ 08_Within_budget/within_budget_V1.py
@@ -3,12 +3,19 @@ from traceback import print_tb
 
 budget = 8
 
-list = [('banana', 'g', '3', '1.50', 0.5), ('Tuy', 'g', '2', '5', 2.5), ('coffee', 'g', '1', '3', 3.0), ('apple', 'g', '1', '9', 9)]
+list = [
+    ("banana", "g", "3", "1.50", 0.5),
+    ("Tuy", "g", "2", "5", 2.5),
+    ("coffee", "g", "1", "3", 3.0),
+    ("apple", "g", "1", "9", 9),
+]
+
 
 def sort_list_budget(list, budget):
     budget = float(budget)
     for item in list:
         cost = float(item[3])
         print(cost)
+
 
 sort_list_budget(list, budget)
