b75773cb9987b8668ccdcf37cbd3ce7dc29ae985
PurelyStupidity <micoolplays@gmail.com>
2022-06-10 20:19:33 +1200

    June 10, 2022 8:19 PM

--- 07_sort_list/sort_list_V1.py
+++ 07_sort_list/sort_list_V1.py
@@ -1,7 +1,7 @@
 list = [("banana", "g", "1", "1.50"),("coffee", "g", "1", "3")]
 
 def calc_price(list):
-    sorted
+    sorted_list = 
     #append to each tuple the price using the price divided by the weight
     for item in list:
         price = float(item[3])
