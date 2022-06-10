27dbc054e4eb42d27d9e55c83dd8723f021facef
PurelyStupidity <micoolplays@gmail.com>
2022-06-10 20:57:05 +1200

    June 10, 2022 8:57 PM

--- 08_Within_budget/within_budget_V1.py
+++ 08_Within_budget/within_budget_V1.py
@@ -1,7 +1,11 @@
+from traceback import print_tb
+
+
 budget = 8
 
 list = [('banana', 'g', '3', '1.50', 0.5), ('Tuy', 'g', '2', '5', 2.5), ('coffee', 'g', '1', '3', 3.0), ('apple', 'g', '1', '9', 9)]
 
 def get_inside_budget(list, budget):
-    for item in list
+    for item in list:
+        print_
 
