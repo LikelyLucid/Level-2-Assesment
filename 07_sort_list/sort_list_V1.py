f9e12de91f4d5b5c15f3764d07aca36bcdfc2f4b
PurelyStupidity <micoolplays@gmail.com>
2022-06-10 20:02:24 +1200

    June 10, 2022 8:02 PM

--- 07_sort_list/sort_list_V1.py
+++ 07_sort_list/sort_list_V1.py
@@ -1,3 +1,7 @@
 list = [("banana", "g", "1", "1.50"),("coffee", "g", "1", "3")]
 
-def calc_price(list)
+def calc_price(list):
+    total = 0
+    for item in list:
+        total += float(item[3])
+    return total
