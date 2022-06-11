00b302158b51d287d09c241cc58afd1c5578e5f0
PurelyStupidity <micoolplays@gmail.com>
2022-06-12 11:49:54 +1200

    June 12, 2022 11:49 AM

--- Main_V1.py
+++ Main_V1.py
@@ -69,3 +69,13 @@ def search_csv(Item):
                 return row
     return None
 
+def sort_list(list):
+    new_list = []
+    for item in list:
+        price = float(item[3])  # convert the price to a float
+        weight = float(item[2])  # convert the weight to a float
+        total_price = price / weight  # calculate the price per weight
+        item = item + (total_price,)  # add the price per weight to the tuple
+        new_list.append(item)  # add the tuple to the list
+    new_list.sort(key=lambda x: x[4])  # sort the list by the price per weight
+    return new_list
