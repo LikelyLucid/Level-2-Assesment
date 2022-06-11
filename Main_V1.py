53305eebf7e2057bc1a1ee68f94c7bdc04dcd150
PurelyStupidity <micoolplays@gmail.com>
2022-06-12 11:48:21 +1200

    June 12, 2022 11:48 AM

--- Main_V1.py
+++ Main_V1.py
@@ -1,19 +1,23 @@
 def check_float(question):
     while True:
         try:
-            float_num = input(question) # ask question
-            return float(float_num) # convert to float and return it
+            float_num = input(question)  # ask question
+            return float(float_num)  # convert to float and return it
 
-        except ValueError: # if not a float then ask again
+        except ValueError:  # if not a float then ask again
             print("Please enter a valid number")
+
 
 def Check_Blank(question):
     while True:
         text = input(question)  # ask question
-        if text != "" and text.isnumeric() == False:  # if text isn't empty then return it
+        if (
+            text != "" and text.isnumeric() == False
+        ):  # if text isn't empty then return it
             return text
         else:  # if text is empty then ask again
             print("Please enter a valid name.")
+
 
 def get_unit():
 
