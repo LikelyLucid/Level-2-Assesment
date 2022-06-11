09ab127d46b9cf00615a9aa7c95fd6dbc4bd4a5e
PurelyStupidity <micoolplays@gmail.com>
2022-06-12 11:59:21 +1200

    June 12, 2022 11:59 AM

---
+++ 06_read_csv/read_csv_V2.py
@@ -0,0 +1,22 @@
+import csv
+
+
+def search_csv(Item):
+    # search for item in Items.csv
+    with open("Items.csv", "r") as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            if row[0] == Item:
+                return row
+    return None
+
+
+print(search_csv("Banana"))
+item = search_csv("Banana")
+if item is None:
+    print("Item not found")
+else:
+    print("item:", item[0])
+    print("unit:", item[1])
+    print("weight:", item[2])
+    print("price:", item[3])
