72888ffba18dd39bfdb3af380d87cc74b8e032c4
PurelyStupidity <micoolplays@gmail.com>
2022-06-12 11:49:28 +1200

    June 12, 2022 11:49 AM

--- Main_V1.py
+++ Main_V1.py
@@ -58,4 +58,5 @@ def append_list(Item, Unit, Weight, Price):
     }  # data is a dictionary
     with open("Items.csv", "a", newline="") as outfile:  # opens file in append mode
         writer = csv.DictWriter(outfile, fieldnames=data.keys())
-        writer.writerow(data)  # writes data to fi
+        writer.writerow(data)  # writes data to file
+
