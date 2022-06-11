a13626ff8853636c8da1d8ff9b45bf55ae2a7c5b
PurelyStupidity <micoolplays@gmail.com>
2022-06-12 11:44:59 +1200

    June 12, 2022 11:44 AM

---
+++ 08_Within_budget/within_budget_V2.py
@@ -0,0 +1,26 @@
+budget = 8
+
+list = [
+    ("banana", "g", "3", "1.50", 0.5),
+    ("Tuy", "g", "2", "5", 2.5),
+    ("coffee", "g", "1", "3", 3.0),
+    ("apple", "g", "1", "9", 9),
+]
+
+
+def sort_list_budget(list, budget):
+    budget = float(budget)
+    within_budget = []
+    outside_budget = []
+    for item in list:
+        cost = float(item[3])
+        if cost > budget:
+            outside_budget.append(item)
+        else:
+           within_budget.append(item)
+    return within_budget, outside_budget
+
+
+within, outside = sort_list_budget(list, budget)
+print(within)
+print(outside)
