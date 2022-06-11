b01ef9675aa0d0fb7bf71a5b6931ec3759e164a3
PurelyStupidity <micoolplays@gmail.com>
2022-06-12 11:53:46 +1200

    June 12, 2022 11:53 AM

--- Main_V1.py
+++ Main_V1.py
@@ -6,7 +6,6 @@ def check_float(question):
         try:
             float_num = input(question)  # ask question
             return float(float_num)  # convert to float and return it
-
         except ValueError:  # if not a float then ask again
             print("Please enter a valid number")
 
