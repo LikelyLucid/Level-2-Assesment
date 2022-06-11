6c82a7ea309a712d53258f28553cc058da67e20e
PurelyStupidity <micoolplays@gmail.com>
2022-06-12 11:45:48 +1200

    June 12, 2022 11:45 AM

--- 08_Within_budget/within_budget_V2.py
+++ 08_Within_budget/within_budget_V2.py
@@ -16,8 +16,8 @@ def sort_list_budget(list, budget):
         if float(item[3]) > budget: # if the cost is greater than the budget
             outside_budget.append(item) # append to the outside budget list
         else:
-           within_budget.append(item)
-    return within_budget, outside_budget 
+           within_budget.append(item) 
+    return within_budget, outside_budget # return the two lists
 
 
 within, outside = sort_list_budget(list, budget)
