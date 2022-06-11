c38d670cba2724dfffc878d0071124601e3770e3
PurelyStupidity <micoolplays@gmail.com>
2022-06-12 11:53:26 +1200

    June 12, 2022 11:53 AM

--- Main_V1.py
+++ Main_V1.py
@@ -92,4 +92,4 @@ def sort_list_budget(list, budget):
            within_budget.append(item) # append to the within budget list
     return within_budget, outside_budget # return the two lists
 
-budget = 
+budget = check_float("Enter the budget: $")
