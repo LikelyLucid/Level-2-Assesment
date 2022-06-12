aac733b4454cb22ea8c4431168ef2020eb6e5b2d
PurelyStupidity <micoolplays@gmail.com>
2022-06-12 15:37:48 +1200

    June 12, 2022 3:37 PM

--- Main_V3.py
+++ Main_V3.py
@@ -138,7 +138,7 @@ while True:
         if choice == "y":  # if the choice is y
             if len(csv_list) == 1:  # if the list has only one item
                 item_list.append(csv_list[0])  # append the item to the item list
-                
+                pg_break()
                 continue
 
             else:  # if the list has more than one item
