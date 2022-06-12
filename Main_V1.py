8a230b33888e7d7007202b108b71d7511349720e
PurelyStupidity <micoolplays@gmail.com>
2022-06-12 12:11:55 +1200

    June 12, 2022 12:11 PM

--- Main_V1.py
+++ Main_V1.py
@@ -106,4 +106,4 @@ while True:
     product_name = Check_Blank("Enter the product name: ").lower()
     if search_csv(product_name) is not None:
         print("\nProduct already exists\n")
-        choice = input("Would you like to autocomplete?")
+        choice = input("Would you like to autocomplete? (y/n): ").lower()
