6b60c7ad630b8d6154cb87b0840cdc4250c0d0e2
PurelyStupidity <micoolplays@gmail.com>
2022-06-12 12:05:49 +1200

    June 12, 2022 12:05 PM

--- Main_V1.py
+++ Main_V1.py
@@ -13,8 +13,7 @@ def check_float(question):
 def Check_Blank(question):
     while True:
         text = input(question)  # ask question
-        if (
-            text != "" and text.isnumeric() == False):  # if text isn't empty then return it
+        if (text != "" and text.isnumeric() == False):  # if text isn't empty then return it
             return text
         else:  # if text is empty then ask again
             print("Please enter a valid name.")
