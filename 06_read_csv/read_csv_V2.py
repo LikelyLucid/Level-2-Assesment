7e392844ec4d70583a8578fbeb50dcd5a5b511e7
PurelyStupidity <micoolplays@gmail.com>
2022-06-12 17:31:16 +1200

    June 12, 2022 5:31 PM

--- 06_read_csv/read_csv_V2.py
+++ 06_read_csv/read_csv_V2.py
@@ -4,14 +4,14 @@ import csv
 def search_csv(Item):
     item_rows = []
     # search for item in Items.csv
-    with open("Items.csv", "r") as csvfile:
+    with open("Items.csv", "r") as csvfile: # opens file in read mode
         reader = csv.reader(csvfile)
         for row in reader: # for each row in the csv file
             if row[0] == Item:
                 item_rows.append(row) # append the row to the list
     if len(item_rows) == 0:
         return None # if no rows are found, return None
-    else: 
+    else: # if rows are found, return the list
         return item_rows
 
 print(search_csv("banana"))
