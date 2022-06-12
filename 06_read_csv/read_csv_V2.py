f91526ea19b0d5da9fc3d7b83a1667c734647231
PurelyStupidity <micoolplays@gmail.com>
2022-06-12 12:03:34 +1200

    June 12, 2022 12:03 PM

--- 06_read_csv/read_csv_V2.py
+++ 06_read_csv/read_csv_V2.py
@@ -10,6 +10,8 @@ def search_csv(Item):
             if row[0] == Item:
                 item_rows.append(row)
     if len(item_rows) == 0:
+        return None
+    else:
     return item_rows
 
 print(search_csv("banana"))
