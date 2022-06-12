b6317784d2baa2225aeef072b553291a0ae8b3a1
PurelyStupidity <micoolplays@gmail.com>
2022-06-12 13:48:31 +1200

    June 12, 2022 1:48 PM

--- Main_V2.py
+++ Main_V2.py
@@ -11,7 +11,7 @@ def check_float(question):
             float_num = console.input(question, style="bold red")  # ask question
             return float(float_num)  # convert to float and return it
         except ValueError:  # if not a float then ask again
-            console.print("Please enter a valid number", style="bold red")
+            console.print("Please enter a valid number")
 
 
 def Check_Blank(question):
