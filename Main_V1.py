ae566e3f5916232d3d44f89639df2397d3352021
PurelyStupidity <micoolplays@gmail.com>
2022-06-12 13:05:36 +1200

    June 12, 2022 1:05 PM

--- Main_V1.py
+++ Main_V1.py
@@ -125,6 +125,6 @@ while True:
                     loop += 1
                 choice = int(input("\nEnter the number of the item: "))
                 item_list.append(csv_list[int(choice)])
-        
+    
 
 print(item_list)
