360d8cc63dc32f814a451848d6211179ee3830bb
PurelyStupidity <micoolplays@gmail.com>
2022-06-12 14:42:23 +1200

    June 12, 2022 2:42 PM

--- Main_V2.py
+++ Main_V2.py
@@ -130,7 +130,7 @@ while True:
                 table.add_column("Option", style="cyan")
                 table.add_column("Name")
                 table.add_column("weight")
-                table.add_column("Price")
+                table.add_column("Price") 
                 for item in csv_list:
                     # print(loop, item)
                     name = item[0]
