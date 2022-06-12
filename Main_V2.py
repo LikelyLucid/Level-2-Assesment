1e1e5b5a4f02b80a9331294adee0d6d8ea442288
PurelyStupidity <micoolplays@gmail.com>
2022-06-12 13:50:50 +1200

    June 12, 2022 1:50 PM

--- Main_V2.py
+++ Main_V2.py
@@ -22,7 +22,7 @@ def Check_Blank(question):
         ):  # if text isn't empty then return it
             return text
         else:  # if text is empty then ask again
-            print("Please enter a valid name.")
+            console.print("Please enter a valid name.")
 
 
 def get_unit():
