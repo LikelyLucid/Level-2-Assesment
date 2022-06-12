3832ddfb1aaffd74d4400d04d32c5fcaa4537b0c
PurelyStupidity <micoolplays@gmail.com>
2022-06-12 17:31:01 +1200

    June 12, 2022 5:31 PM

--- 06_read_csv/read_csv_V2.py
+++ 06_read_csv/read_csv_V2.py
@@ -6,7 +6,7 @@ def search_csv(Item):
     # search for item in Items.csv
     with open("Items.csv", "r") as csvfile:
         reader = csv.reader(csvfile)
-        for row in reader:
+        for row in reader: 
             if row[0] == Item:
                 item_rows.append(row)
     if len(item_rows) == 0:
