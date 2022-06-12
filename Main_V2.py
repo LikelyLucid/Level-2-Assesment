ac0462792e01d6f9d8f1ccfb016f5e2eb146afbe
PurelyStupidity <micoolplays@gmail.com>
2022-06-12 14:39:09 +1200

    June 12, 2022 2:39 PM

--- Main_V2.py
+++ Main_V2.py
@@ -136,7 +136,7 @@ while True:
                     name = item[0]
                     weight = str(item[2]) + " " + item[1]
                     price = "$" + str(item[3])
-                    table.add_row("Dec 20, 2019", "Star Wars: The Rise of Skywalker", "$952,110,690")
+                    table.add_row()
 
                     loop += 1
                 choice = int(input("\nEnter the number of the item: "))
