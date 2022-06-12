cd1870362531c2be8e4aadacdedb5c93e0d74252
PurelyStupidity <micoolplays@gmail.com>
2022-06-12 13:09:10 +1200

    June 12, 2022 1:09 PM

--- Main_V1.py
+++ Main_V1.py
@@ -127,5 +127,6 @@ while True:
                 item_list.append(csv_list[int(choice)])
     else: # if the product is not in the csv
         unit, amount = get_unit()
+        price 
 
 print(item_list)
