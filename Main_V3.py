0076e6af82057c8440cc39f181e7fce6250ad76c
PurelyStupidity <micoolplays@gmail.com>
2022-06-12 16:41:38 +1200

    June 12, 2022 4:41 PM

--- Main_V3.py
+++ Main_V3.py
@@ -119,7 +119,7 @@ def final_table(Title, list): # create a table with the list
     table.add_column("Price")
     table.add_column("Price per Kg")
     for item in list:
-        table.add_row(str(item[0]), str(item[2]), str(item[3]), str(round(item[4], 2))) # Name Weight Price Price per Kg
+        table.add_row(str(item[0]), str(item[2]), str(item[3]), str(round(item[4], 2))) # Name, Weight Price Price per Kg
     console.print(table) # print the table
 
 budget = check_float("Enter the budget: $ ")
